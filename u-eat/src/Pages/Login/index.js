75bd6ee561870a89cb1fba886850faf04d78509e
Alejandro R <alejandro-ramirez27@hotmail.com>
2022-06-20 12:21:37 -0600

    Front-end: Login, Se agregó el logo

--- u-eat/src/Pages/Login/index.js
+++ u-eat/src/Pages/Login/index.js
@@ -2,9 +2,9 @@ function Login() {
 
     return (
         <>
-            <div className ="flex items-center justify-center bg-fondoPrincipal ">
+            <div className ="flex items-center justify-center bg-fondoPrincipal  h-screen">
                 < div className ="text-center">
-                    <img className=" h-[300px] w-[300px] m-10"src="https://c8.alamy.com/compes/2a6xjfe/vector-logo-de-comida-rapida-pegatina-con-juego-de-hamburguesa-de-pollo-fresco-con-chuleta-frita-y-ensalada-patatas-fritas-en-caja-de-carton-de-las-bebidas-gaseosas-en-plastico-2a6xjfe.jpg"/>
+                    <img className=" h-[300px] w-[300px] m-10"src="https://i.postimg.cc/PJ0RkZqJ/logo512.png"/>
                     <div className="">
                         <input
                         placeholder = "Correo"

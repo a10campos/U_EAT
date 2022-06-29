8d677ec1943903ca199f606e6b67129b5e109d4d
a10campos <a10.campos@hotmail.com>
2022-06-29 13:47:20 -0600

    Fix: fixed cors

--- backendUeat/server.js
+++ backendUeat/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const cors = require("cors");
 dotenv.config();
 const server = express();
 server.use(express.json());
+server.use(cors());
 server.set('json spaces',2);
 
 const userRoutes = require("./routes/user");
